d10edbd0d43ca641d373d0d014724bdd7b67ae42
Bernard Mc Clement <gen@mcclement.co.uk>
2024-10-21 16:44:50 +1100

    update to support Dragonfish

--- check_asustor_it87.kmod.sh
+++ check_asustor_it87.kmod.sh
@@ -1,39 +1,41 @@
 #!/bin/sh
 
-# Asustor Flashstor kernel module check/compile script for TrueNAS-SCALE
-# Checks to see if the necessary it87 kmod exists, installs it if not, and runs the fan control script
-# By Bernard Mc Clement, Sept 2023
+: <<'EOF' #Introductory comment block:
+Asustor Flashstor kernel module check/compile script for TrueNAS-SCALE
+Checks to see if the necessary it87 kmod exists, installs it if not, and runs the fan control script
+By Bernard Mc Clement, Sept 2023. 
+Updated 10/2024 to support Dragonfish
+
+Note that you can check if the asustor-it87 kmod is installed by running the following command in the truenas shell:
+ if lsmod | grep -q asustor_it87; then
+    echo "asustor-it87 kmod is already installed."
+  else
+    echo "asustor-it87 kmod not found or not installed."
+ fi
+EOF
+
 
 # Add this as a post-init script so that it runs on every boot
 
 # Check if the kmod exists and is installed
-if ! modinfo asustor-it87 >/dev/null 2>&1; then
+if ! lsmod | grep -q asustor_it87; then
     echo "asustor-it87 kmod not found or not installed. Compiling and installing..."
 
     # Clone the repository
     git clone https://github.com/mafredri/asustor-platform-driver
     cd asustor-platform-driver
 
-    # Checkout the it87 branch
-    git checkout it87
+    # Install dkms
+    sudo apt install -y dkms
 
     # Compile the kmod
     sudo make
 
-    # Install the kmod
-    sudo make install
+    # Install the kmod using dkms
+    sudo make dkms
 
-    # Update module dependencies
-    sudo depmod -a
-
-    # Load the module
-    sudo modprobe -a asustor_it87
-
-    echo "asustor-it87 kmod compiled, installed, and loaded successfully."
+    echo "asustor-it87 kmod compiled and installed successfully."
 else
-    # Load the module
-    sudo modprobe -a asustor_it87
-
     echo "asustor-it87 kmod is already installed."
 fi
 
